c1ee5755494f339161fdb6b4f09cd52063396076
yaakov gamliel <yaakov@sergata.mobi>
2014-02-06 13:42:32 +0200

    first commit

---
+++ HereWeTest/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  HereWeTest
+//
+//  Created by yaakov on 2/6/14.
+//  Copyright (c) 2014 yaakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+@end
---
+++ HereWeTest/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  HereWeTest
+//
+//  Created by yaakov on 2/6/14.
+//  Copyright (c) 2014 yaakov. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
