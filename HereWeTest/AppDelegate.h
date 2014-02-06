7b9155b960e987dbaba0784d6e69975e564d5ebf
yaakov gamliel <yaakov@sergata.mobi>
2014-02-06 12:17:45 +0200

    Initial Commit

---
+++ HereWeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HereWeTest
+//
+//  Created by yaakov on 2/6/14.
+//  Copyright (c) 2014 yaakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
